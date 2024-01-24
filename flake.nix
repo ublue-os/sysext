ff8343161b563479d1ace0ab78a0e5b5627affe7
Tulili <tulilirockz@outlook.com>
2024-01-24 15:48:02 -0300

    feat: add sysext-image maker straight into flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "nixpkgs/release-23.11";
     flake-utils = {
       url = "github:numtide/flake-utils";
     };
@@ -18,7 +18,6 @@
         pkgs = import nixpkgs {inherit system;};
         lib = pkgs.lib;
         config = pkgs.lib.trivial.importJSON ./config.json;
-        wrapScript = name: pkgs.writeShellScriptBin name (builtins.readFile ./${name});
         all_deps =
           (builtins.map (package: pkgs.${package}) config.packages)
           ++ (lib.lists.flatten (builtins.map (package: pkgs.${package}.buildInputs) config.packages));
@@ -50,8 +49,8 @@
             };
         };
         packages = {
-          default = self.packages.${system}.symlinks-for-sysext;
-          symlinks-for-sysext = pkgs.symlinkJoin {
+          default = self.packages.${system}.sysext-derivation;
+          sysext-derivation = pkgs.symlinkJoin {
             name = "derivation-with-config.json-outputs";
             paths = all_deps;
             meta.priority = 10;
@@ -70,12 +69,40 @@
               } > "$out/lib/extension-release.d/extension-release.${config.sysext-name}.sysext"
             '';
           };
-          squashfs-maker = pkgs.symlinkJoin {
-            name = "sysext.sh";
-            paths = [(wrapScript "sysext.sh")] ++ (with pkgs; [squashfsTools btrfs-progs]);
-            buildInputs = [pkgs.makeWrapper];
-            postBuild = "wrapProgram $out/bin/sysext.sh --prefix PATH : $out/bin";
-          };
+          sysext-image-maker = pkgs.writeShellScriptBin "makeSysext.sh" ''
+            # got this script from flatcar linux bakery!
+            FORMAT="$1"
+            DIRECTORY="$2"
+            TARGET_NAME="$3"
+            if [ $# -lt 3 ]; then
+              echo "Usage: $0 FORMAT DIRECTORY"
+              echo "The script will make a TARGET_NAME.raw image of the folder DIRECTORY with FORMAT format."
+              exit 1
+            fi
+            if [ "$FORMAT" != "squashfs" ] && [ "$FORMAT" != "btrfs" ] && [ "$FORMAT" != "ext4" ] && [ "$FORMAT" != "ext2" ]; then
+              echo "Expected FORMAT=squashfs, FORMAT=btrfs, FORMAT=ext4, or FORMAT=ext2, got '$FORMAT'" >&2
+            exit 1
+            fi
+            
+            if [ "$FORMAT" = "btrfs" ]; then
+              # Note: We didn't chown to root:root, meaning that the file ownership is left as is
+              ${pkgs.btrfs-progs}/bin/mkfs.btrfs --mixed -m single -d single --shrink --rootdir "$DIRECTORY" "$DIRECTORY".raw
+              # This is for testing purposes and makes not much sense to use because --rootdir doesn't allow to enable compression
+            elif [ "$FORMAT" = "ext4" ] || [ "$FORMAT" = "ext2" ]; then
+              # Assuming that 1 GB is enough
+              ${pkgs.coreutils}/bin/truncate -s 1G "$DIRECTORY".raw
+              # Note: We didn't chown to root:root, meaning that the file ownership is left as is
+              ${pkgs.e2fsprogs}/bin/mkfs."$FORMAT" -E root_owner=0:0 -d "$DIRECTORY" "$DIRECTORY".raw
+              ${pkgs.e2fsprogs}/bin/resize2fs -M "$DIRECTORY".raw
+            else
+              ${pkgs.squashfsTools}/bin/mksquashfs "$DIRECTORY" "$TARGET_NAME".raw -all-root
+            fi
+            '';
+          
+          compile-configuration = pkgs.writeShellScriptBin "compiler.sh" ''
+            ${lib.getExe pkgs.nixStatic} bundle -L --bundler .#bundle-all-config -o cool_thing.tar &
+            ${lib.getExe self.packages.${system}.sysext-image-maker} squashfs ${self.packages.${system}.sysext-derivation} cool_thing.sysext
+          '';
         };
       }
     );
