799b54cfa74ad52e4cfc33f00eb808e28c54da74
Tulili <tulilirockz@outlook.com>
2024-01-24 20:20:20 -0300

    feat: compiler for configuration.json + script that wraps everything and mounts sysext

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "Systemd System Extensions using the Nix Store";
 
   inputs = {
     nixpkgs.url = "nixpkgs/release-23.11";
@@ -18,39 +18,38 @@
         pkgs = import nixpkgs {inherit system;};
         lib = pkgs.lib;
         config = pkgs.lib.trivial.importJSON ./config.json;
-        all_deps =
-          (builtins.map (package: pkgs.${package}) config.packages)
-          ++ (lib.lists.flatten (builtins.map (package: pkgs.${package}.buildInputs) config.packages));
-        tarfile-buildscript = ''
+        all_deps = (builtins.map (package: pkgs.${package}) config.packages);
+          #++ (lib.lists.flatten (builtins.map (package: pkgs.${package}.buildInputs) config.packages));
+        squashfs-build = ''
           storePaths=$(${lib.getExe pkgs.perl} ${pkgs.pathsFromGraph} ./closure-*)
 
-          tar -cf - \
-            --owner=0 --group=0 --mode=u+rw,uga+r \
-            --hard-dereference \
-            $storePaths > $out
+          ${pkgs.squashfsTools}/bin/mksquashfs \
+            $storePaths \
+            $out \
+            -all-root -no-hardlinks -noDataCompression -exit-on-error
         '';
       in {
         formatter = pkgs.alejandra;
         bundlers = {
           toTar = {...} @ drv:
             pkgs.stdenv.mkDerivation {
-              name = drv.pname + "-store.tar";
+              name = drv.pname + "-store.sqfs";
               buildInputs = [pkgs.perl pkgs.gnutar];
               exportReferencesGraph = lib.lists.flatten (builtins.map (x: [("closure-" + baseNameOf x) x]) (lib.lists.flatten (pkgs.${drv.pname})));
-              buildCommand = tarfile-buildscript;
+              buildCommand = squashfs-build;
             };
 
           bundle-all-config = _:
             pkgs.stdenv.mkDerivation {
-              name = config.sysext-name + "-store.tar";
+              name = config.sysext-name + "-store.sqfs";
               buildInputs = [pkgs.perl pkgs.gnutar];
               exportReferencesGraph = lib.lists.flatten (builtins.map (x: [("closure-" + baseNameOf x) x]) all_deps);
-              buildCommand = tarfile-buildscript;
+              buildCommand = squashfs-build;
             };
         };
         packages = {
-          default = self.packages.${system}.sysext-derivation;
-          sysext-derivation = pkgs.symlinkJoin {
+          default = self.packages.${system}.compile-configuration;
+          sysext-derivation-from-config = pkgs.symlinkJoin {
             name = "derivation-with-config.json-outputs";
             paths = all_deps;
             meta.priority = 10;
@@ -67,6 +66,8 @@
                   echo "ARCHITECTURE=${config.arch}"
                 fi
               } > "$out/lib/extension-release.d/extension-release.${config.sysext-name}.sysext"
+              mkdir -p $out/usr
+              ${lib.getExe pkgs.findutils} $out -maxdepth 1 -type d -exec mv {} $out/usr/ \;
             '';
           };
           sysext-image-maker = pkgs.writeShellScriptBin "makeSysext.sh" ''
@@ -83,7 +84,7 @@
               echo "Expected FORMAT=squashfs, FORMAT=btrfs, FORMAT=ext4, or FORMAT=ext2, got '$FORMAT'" >&2
             exit 1
             fi
-            
+
             if [ "$FORMAT" = "btrfs" ]; then
               # Note: We didn't chown to root:root, meaning that the file ownership is left as is
               ${pkgs.btrfs-progs}/bin/mkfs.btrfs --mixed -m single -d single --shrink --rootdir "$DIRECTORY" "$DIRECTORY".raw
@@ -97,11 +98,29 @@
             else
               ${pkgs.squashfsTools}/bin/mksquashfs "$DIRECTORY" "$TARGET_NAME".raw -all-root
             fi
-            '';
-          
+          '';
+            
           compile-configuration = pkgs.writeShellScriptBin "compiler.sh" ''
-            ${lib.getExe pkgs.nixStatic} bundle -L --bundler .#bundle-all-config -o cool_thing.tar &
-            ${lib.getExe self.packages.${system}.sysext-image-maker} squashfs ${self.packages.${system}.sysext-derivation} cool_thing.sysext
+            SYSEXT_NAME="$1"
+            echo "the system extension name should be the same name as the .sysext file in the metadata!"
+            ${lib.getExe self.packages.${system}.sysext-image-maker} \
+              squashfs \
+              ${self.packages.${system}.sysext-derivation-from-config} \
+              ${config.sysext-name}.sysext
+            ${lib.getExe pkgs.nixStatic} bundle -L --bundler .#bundle-all-config -o ${config.sysext-name}-store.sqfs
+          '';
+
+          mount-squashfs = pkgs.writeShellScriptBin "squashfs-mounter.sh" ''
+            TARGET_SQUASHFS_NIX="$1"
+            if [ ! -e /nix ] ; then
+              chattr +i /
+              mkdir /nix
+              chattr -i /
+            fi
+            mkdir -p /tmp/nix-store-bindmount
+            mount --bind /nix/store /tmp/nix-store-bindmount
+            mount --bind /tmp/nix-store-bindmount /nix/store
+            mount -t squashfs -o loop $TARGET_SQUASHFS_NIX /nix/store
           '';
         };
       }
